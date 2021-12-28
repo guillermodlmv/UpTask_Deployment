90a5825c0d61f6225b10934ef9814d2e0a103d22
Guillermo Villagomez <gvdelamora@gmail.com>
2021-12-28 13:18:22 -0600

    json

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config({path:'variables.env'})
+
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NOMBRE } = process.env;
 const db = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NOMBRE}`, {
   logging: false, // set to console.log to see the raw SQL queries
