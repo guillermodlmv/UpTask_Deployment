9bba3b888419960031874f16e6b77dcb8106743c
Guillermo Villagomez <gvdelamora@gmail.com>
2021-12-28 15:26:57 -0600

    tareas bug

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config({path:'variables.env'})
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NOMBRE } = process.env;
-const db = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NOMBRE}`, {
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NOMBRE, DB_TYPE } = process.env;
+const db = new Sequelize(`${DB_TYPE}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NOMBRE}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
