33cf0e2fbcbb9f53c8eb97762861259bf98db466
Guillermo Villagomez <gvdelamora@gmail.com>
2021-12-28 14:45:53 -0600

    x

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config({path:'variables.env'})
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NOMBRE } = process.env;
-const db = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NOMBRE}`, {
+const db = new Sequelize(`mysql://bc9ccf6f19cfda:36404e19@us-cdbr-east-05.cleardb.net/heroku_12ed9e9c1ac7f72`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
